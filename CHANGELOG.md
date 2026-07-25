29d11e05e4ece2faa8f96a33c1dd60601662e661
Leonid Groshev <OtezVikentiy@gmail.com>
2026-07-25 23:53:41 +0300

    release: v0.3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,8 @@ once tagged releases begin.
 
 ## [Unreleased]
 
+## [0.3.1] - 2026-07-25
+
 ## [0.3.0] - 2026-07-24
 
 ### Added
--- internal/version/version.go
+++ internal/version/version.go
@@ -15,7 +15,7 @@ import (
 	"strings"
 )
 
-const base = "0.3.0"
+const base = "0.3.1"
 
 var (
 	version = "" // git describe --tags --always --dirty (через ldflags)
